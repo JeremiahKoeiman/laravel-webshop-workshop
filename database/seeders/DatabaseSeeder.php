c3a4bb691fb925a870c9ff67ebade15afb1eab14
Jeremiah <50832578+JeremiahKoeiman@users.noreply.github.com>
2020-09-25 14:19:04 +0200

    Roles and Permissions added

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,6 +13,8 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
+        $this->call(RoleAndPermissionSeeder::class);
+        $this->call(UserSeeder::class);
         $this->call(CategorySeeder::class);
     }
 }
---
+++ database/seeders/RoleAndPermissionSeeder.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Permission;
+use Spatie\Permission\Models\Role;
+
+class RoleAndPermissionSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        app()['cache']->forget('spatie.permission.cache');
+
+        Permission::create(['name' => 'create category']);
+        Permission::create(['name' => 'edit category']);
+        Permission::create(['name' => 'delete category']);
+
+        $role = Role::create(['name' => 'customer']);
+
+        $role = Role::create(['name' => 'sales']);
+        $role->givePermissionTo('create category', 'edit category');
+
+        $role = Role::create(['name' => 'admin']);
+        $role->givePermissionTo(Permission::all());
+    }
+}
