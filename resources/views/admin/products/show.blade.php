cff50eacec91b327f117e44fac8c37eb2b5896f0
Jeremiah <50832578+JeremiahKoeiman@users.noreply.github.com>
2020-09-26 14:43:03 +0200

    Show done

--- resources/views/admin/products/show.blade.php
+++ resources/views/admin/products/show.blade.php
@@ -1,28 +1,30 @@
 @extends('layouts.layout')
 
 @section('content')
-    <h1 class="mt-5">Categories</h1>
+    <h1 class="mt-5">Products</h1>
 
     <nav class="nav">
         <ul class="nav nav-tabs">
             <li class="nav-item">
-                <a href="{{ route('categories.index') }}" class="nav-link">Index</a>
+                <a href="{{ route('products.index') }}" class="nav-link">Index</a>
             </li>
             <li class="nav-item">
-                <a href="{{ route('categories.create') }}" class="nav-link ">Create</a>
+                <a href="{{ route('products.create') }}" class="nav-link ">Create</a>
             </li>
             <li class="nav-item">
-                <a href="{{ route('categories.show', ['category' => $category->id]) }}" class="nav-link active">Category details</a>
+                <a href="{{ route('products.show', ['product' => $product->id]) }}" class="nav-link active">Product details</a>
             </li>
         </ul>
     </nav>
 
     <div class="card">
         <div class="card-header">
-            Categories
+            Product
         </div>
         <div class="card-body">
-            <h2 class="card-title">{{ $category->name }}</h2>
+            <h2 class="card-title">{{ $product->name }}</h2>
+            <p class="card-text">{{ $product->description }}</p>
+            <p class="card-text">€ {{ $product->get_latest_price->price }}</p>
         </div>
     </div>
 @endsection
