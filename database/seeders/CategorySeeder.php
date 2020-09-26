ec5cb7a8c16af491abd30ab87a0a7ed01a12d2c6
Jeremiah <50832578+JeremiahKoeiman@users.noreply.github.com>
2020-09-26 13:37:05 +0200

    PriceFactory added to CategorySeeder

--- database/seeders/CategorySeeder.php
+++ database/seeders/CategorySeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use App\Models\Category;
+use App\Models\Price;
 use App\Models\Product;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
@@ -21,8 +22,14 @@ class CategorySeeder extends Seeder
             ->times(3)
             ->create()
             ->each(function ($category) {
-                $category->product()->saveMany(Product::factory()->times(10)->create(['category_id' => $category->id]));
-            })
-        ;
+                $category->product()->saveMany(Product::factory()->times(10)
+                    ->create(['category_id' => $category->id])
+                    ->each(function ($product) {
+                        $product->price()->saveMany(Price::factory()->times(3)
+                            ->create(['product_id' => $product->id])
+                        );
+                    })
+                );
+            });
     }
 }
