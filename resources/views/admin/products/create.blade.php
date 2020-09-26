cedbd143ad63b2f01970481cd7c438023f2b1555
Jeremiah <50832578+JeremiahKoeiman@users.noreply.github.com>
2020-09-26 14:42:52 +0200

    Create done

--- resources/views/admin/products/create.blade.php
+++ resources/views/admin/products/create.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.layout')
 
 @section('content')
-    <h1 class="mt-5">Categories</h1>
+    <h1 class="mt-5">Products</h1>
 
     @if($errors->any())
         <div class="alert alert-danger">
@@ -16,22 +16,44 @@
     <nav class="nav">
         <ul class="nav nav-tabs">
             <li class="nav-item">
-                <a href="{{ route('categories.index') }}" class="nav-link">Index</a>
+                <a href="{{ route('products.index') }}" class="nav-link">Index</a>
             </li>
             <li class="nav-item">
-                <a href="{{ route('categories.create') }}" class="nav-link active">Create</a>
+                <a href="{{ route('products.create') }}" class="nav-link active">Create</a>
             </li>
         </ul>
     </nav>
 
-    <form method="POST" action="{{ route('categories.store') }}">
+    <form method="POST" action="{{ route('products.store') }}">
         @csrf
 
         <div class="form-group">
-            <label for="name">Category name</label>
+            <label for="name">Product name</label>
             <input type="text" name="name" class="form-control" id="name"
-                   aria-describedby="categorienameHelp" placeholder="Enter category name">
+                   aria-describedby="productnameHelp" placeholder="Enter category name">
         </div>
+
+        <div class="form-group">
+            <label for="description">Product description</label>
+            <textarea name="description" class="form-control" id="description"
+                      rows="3" placeholder="Enter product description"></textarea>
+        </div>
+
+        <div class="form-group">
+            <label for="price">Product price</label>
+            <input type="text" name="price" class="form-control" id="price"
+                   aria-describedby="productpriceHelp" placeholder="Enter product price">
+        </div>
+
+        <div class="form-group">
+            <label for="category_id">Select category</label>
+            <select name="category_id" id="category_id" class="form-control">
+                @foreach($categories as $category)
+                    <option value="{{ $category->id }}">{{ $category->name }}</option>
+                @endforeach
+            </select>
+        </div>
+
         <button type="submit" class="btn btn-primary">Submit</button>
     </form>
 @endsection
