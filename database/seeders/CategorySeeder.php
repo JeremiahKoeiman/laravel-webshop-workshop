3610dcdb9b59a27ed4b01da99bdd4d4811315aec
Jeremiah <50832578+JeremiahKoeiman@users.noreply.github.com>
2020-09-26 13:23:31 +0200

    Category seeder modified

--- database/seeders/CategorySeeder.php
+++ database/seeders/CategorySeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use App\Models\Category;
+use App\Models\Product;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
 
@@ -16,6 +17,12 @@ class CategorySeeder extends Seeder
     public function run()
     {
         //
-        Category::factory()->times(10)->create();
+        Category::factory()
+            ->times(3)
+            ->create()
+            ->each(function ($category) {
+                $category->product()->saveMany(Product::factory()->times(10)->create(['category_id' => $category->id]));
+            })
+        ;
     }
 }
